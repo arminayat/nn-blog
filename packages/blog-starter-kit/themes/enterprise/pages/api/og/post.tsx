75885c7fc533e82788e9990f99966c036498c0f3
Armin <110829799+arminayat-tm@users.noreply.github.com>
2024-09-22 13:13:16 +0200

    Add Roboto Slab font integration and update font references

--- packages/blog-starter-kit/themes/enterprise/pages/api/og/post.tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/api/og/post.tsx
@@ -8,23 +8,23 @@ export const config = {
 };
 
 const fontRegular = fetch(
-	new URL('../../../assets/PlusJakartaSans-Regular.ttf', import.meta.url),
+	new URL('../../../assets/Roboto_Slab/static/RobotoSlab-Regular.ttf', import.meta.url),
 ).then((res) => res.arrayBuffer());
 
 const fontMedium = fetch(
-	new URL('../../../assets/PlusJakartaSans-Medium.ttf', import.meta.url),
+	new URL('../../../assets/Roboto_Slab/static/RobotoSlab-Medium.ttf', import.meta.url),
 ).then((res) => res.arrayBuffer());
 
 const fontSemiBold = fetch(
-	new URL('../../../assets/PlusJakartaSans-SemiBold.ttf', import.meta.url),
+	new URL('../../../assets/Roboto_Slab/static/RobotoSlab-SemiBold.ttf', import.meta.url),
 ).then((res) => res.arrayBuffer());
 
-const fontBold = fetch(new URL('../../../assets/PlusJakartaSans-Bold.ttf', import.meta.url)).then(
+const fontBold = fetch(new URL('../../../assets/Roboto_Slab/static/RobotoSlab-Bold.ttf', import.meta.url)).then(
 	(res) => res.arrayBuffer(),
 );
 
 const fontExtraBold = fetch(
-	new URL('../../../assets/PlusJakartaSans-ExtraBold.ttf', import.meta.url),
+	new URL('../../../assets/Roboto_Slab/static/RobotoSlab-ExtraBold.ttf', import.meta.url),
 ).then((res) => res.arrayBuffer());
 
 export default async function handler(req: NextRequest) {
@@ -67,7 +67,7 @@ export default async function handler(req: NextRequest) {
 		(
 			<div
 				style={{
-					fontFamily: '"Plus Jakarta Sans"',
+					fontFamily: '"Roboto Slab"',
 					backgroundColor: bannerBackground,
 				}}
 				tw="relative flex h-full w-full flex-col p-8 subpixel-antialiased"
---
+++ packages/blog-starter-kit/themes/enterprise/styles/fonts.ts
@@ -0,0 +1,6 @@
+import { Roboto_Slab } from "next/font/google";
+
+export const fontRobotoSlab = Roboto_Slab({
+  subsets: ["latin"],
+  variable: "--font-roboto-slab",
+});
